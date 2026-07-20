f71aba9e269c5cd480eb006096e1b55bd7e2b788
Nguyen Trung Nghia <nghia011104@gmail.com>
2026-07-20 12:50:45 +0700

    Chinh sua lai cach hien gia tri moi + cu

--- src/SmartPOS.WPF/ViewModels/AuditLogViewModel.cs
+++ src/SmartPOS.WPF/ViewModels/AuditLogViewModel.cs
@@ -7,10 +7,13 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Collections.Generic;
 using SmartPOS.WPF.Navigation;
 using SmartPOS.WPF.Session;
 
 namespace SmartPOS.WPF.ViewModels;
+
+public record AuditLogDetailItem(string Attribute, string? OldValue, string? NewValue);
 
 public partial class AuditLogViewModel : ObservableObject
 {
@@ -48,7 +51,84 @@ public partial class AuditLogViewModel : ObservableObject
     public AuditLog? SelectedLog
     {
         get => _selectedLog;
-        set => SetProperty(ref _selectedLog, value);
+        set 
+        {
+            if (SetProperty(ref _selectedLog, value))
+            {
+                OnSelectedLogChanged(value);
+            }
+        }
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<AuditLogDetailItem> _selectedLogDetails = new();
+
+    private void OnSelectedLogChanged(AuditLog? value)
+    {
+        SelectedLogDetails.Clear();
+
+        if (value == null) return;
+
+        var isAdded = value.Action == "Added";
+        var isModified = value.Action == "Modified";
+
+        var oldValues = new Dictionary<string, string>();
+        var newValues = new Dictionary<string, string>();
+
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(value.OldValue))
+            {
+                var dict = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, System.Text.Json.JsonElement>>(value.OldValue);
+                if (dict != null)
+                {
+                    foreach (var kvp in dict)
+                        oldValues[kvp.Key] = kvp.Value.ToString();
+                }
+            }
+        }
+        catch { }
+
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(value.NewValue))
+            {
+                var dict = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, System.Text.Json.JsonElement>>(value.NewValue);
+                if (dict != null)
+                {
+                    foreach (var kvp in dict)
+                        newValues[kvp.Key] = kvp.Value.ToString();
+                }
+            }
+        }
+        catch { }
+
+        var allKeys = oldValues.Keys.Union(newValues.Keys).Distinct().OrderBy(k => k);
+
+        foreach (var key in allKeys)
+        {
+            var oldVal = oldValues.TryGetValue(key, out var o) ? o : null;
+            var newVal = newValues.TryGetValue(key, out var n) ? n : null;
+
+            if (isAdded)
+            {
+                if (newVal != null)
+                {
+                    SelectedLogDetails.Add(new AuditLogDetailItem(key, null, newVal));
+                }
+            }
+            else if (isModified)
+            {
+                if (oldVal != newVal)
+                {
+                    SelectedLogDetails.Add(new AuditLogDetailItem(key, oldVal, newVal));
+                }
+            }
+            else 
+            {
+                SelectedLogDetails.Add(new AuditLogDetailItem(key, oldVal, newVal));
+            }
+        }
     }
 
     private string _filterQuery = string.Empty;
